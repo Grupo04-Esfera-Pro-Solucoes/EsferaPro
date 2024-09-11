0ff706dbaa1f171c8e5420e46ad9f267e56d0ce9
Igorggwp <igorggwpp@gmail.com>
2024-09-11 20:30:33 -0300

    finalizado ajuda

--- lib/screens/ajuda.dart
+++ lib/screens/ajuda.dart
@@ -6,92 +6,9 @@ class AjudaPage extends StatefulWidget {
 }
 
 class _AjudaPageState extends State<AjudaPage> {
-
-  final List<bool> _expandedStates = List.generate(6, (_) => false);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: ListView.builder(
-          itemCount: faq.length,
-          itemBuilder: (context, index) {
-            return Container(
-              margin: const EdgeInsets.symmetric(vertical: 8.0),
-              decoration: BoxDecoration(
-                border: Border.all(color: Color(0xFF98A2B3), width: 1.0),
-                borderRadius: BorderRadius.circular(15.0),
-              ),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(15.0),
-                child: Column(
-                  children: <Widget>[
-                    GestureDetector(
-                      onTap: () {
-                        setState(() {
-                          _expandedStates[index] = !_expandedStates[index];
-                        });
-                      },
-                      child: Container(
-                        padding: const EdgeInsets.symmetric(
-                          horizontal: 16.0,
-                          vertical: 12.0,
-                        ),
-                        color: Colors.white,
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: <Widget>[
-                            Expanded(
-                              child: Text(
-                                faq[index]['question']!,
-                                style: TextStyle(
-                                  fontSize: 18.0,
-                                  fontWeight: FontWeight.bold,
-                                  color: Color(0xFF6502D4),
-                                ),
-                              ),
-                            ),
-                            Icon(
-                              _expandedStates[index]
-                                  ? Icons.arrow_drop_up
-                                  : Icons.arrow_drop_down,
-                              color: Color(0xFF6502D4),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                    if (_expandedStates[index])
-                      Container(
-                        color: Colors.grey[200],
-                        padding: const EdgeInsets.symmetric(
-                          horizontal: 16.0,
-                          vertical: 8.0,
-                        ),
-                        child: Text(
-                          faq[index]['answer']!,
-                          style: TextStyle(
-                            fontSize: 16.0,
-                          ),
-                        ),
-                      ),
-                  ],
-                ),
-              ),
-            );
-          },
-        ),
-      ),
-    );
-  }
+  List<bool> _expanded = List.generate(6, (_) => false);
 
   final List<Map<String, String>> faq = [
-    {
-      'question': 'Qual o melhor sistema CRM?',
-      'answer': 'É o EsferaPro Soluções!',
-    },
     {
       'question': 'Esqueci minha senha, o que faço?',
       'answer': 'Clique em "Esqueceu a senha?" na tela de login e siga as instruções para redefinir sua senha.',
@@ -110,11 +27,89 @@ class _AjudaPageState extends State<AjudaPage> {
     },
     {
       'question': 'Posso alterar as configurações do aplicativo?',
-      'answer': 'Sim, vá até a aba "Configurações" no menu lateral para ajustar as preferências do aplicativo.',
+      'answer': 'Sim, vá até o icone "Perfil" na barra superior para ajustar as preferências do aplicativo.',
     },
     {
-      'question': 'Qual a equipe responsavel pelo projeto?',
-      'answer': 'Gustavo Dechotti, Heloisa Hartmann, Igor Costa, João Hass, Nathan Breier',
+      'question': 'Quem foi a equipe responsavel pela criação do aplicativo?',
+      'answer': 'Os membros da equipe são: Gustavo Dechotti, Heloisa Hartmann, Igor Costa, João Haas, Nathan Breier',
     },
   ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          itemCount: faq.length,
+          itemBuilder: (context, index) {
+            return Container(
+              margin: const EdgeInsets.symmetric(vertical: 8.0),
+              decoration: BoxDecoration(
+                border: Border.all(color: Color(0xFF98A2B3), width: 1.0),
+                borderRadius: BorderRadius.circular(15.0), 
+              ),
+              child: Column(
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        _expanded[index] = !_expanded[index];
+                      });
+                    },
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.all(Radius.circular(15.0)),
+                      ),
+                      padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 16.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Expanded(
+                            child: Text(
+                              faq[index]['question']!,
+                              style: TextStyle(
+                                fontSize: 18.0,
+                                fontWeight: FontWeight.bold,
+                                color: Color(0xFF6502D4),
+                              ),
+                            ),
+                          ),
+                          Icon(
+                            _expanded[index] ? Icons.arrow_drop_up : Icons.arrow_drop_down,
+                            color: Color(0xFF6502D4),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  AnimatedContainer(
+                    duration: const Duration(milliseconds: 300),
+                    height: _expanded[index] ? null : 0,
+                    decoration: BoxDecoration(
+                      color: Colors.grey[200],
+                      borderRadius: BorderRadius.vertical(bottom: Radius.circular(15.0)),
+                    ),
+                    child: _expanded[index]
+                        ? Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+                            child: Text(
+                              faq[index]['answer']!,
+                              style: TextStyle(
+                                fontSize: 16.0,
+                              ),
+                            ),
+                          )
+                        : null,
+                  ),
+                ],
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
 }
