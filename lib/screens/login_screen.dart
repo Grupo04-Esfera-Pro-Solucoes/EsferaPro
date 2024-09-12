13df6390ee32fbe65894f377bb72c4e83ea89c4b
Gustavo Dechotti <75210025+GusDechotti@users.noreply.github.com>
2024-09-12 20:18:16 -0300

    7 autenticao de login (#21)
    * Post funcionando sem os apontamentos de paginas
    
    * Alteracoes COM apontamentos
    
    * Tratamento de erros
    
    * Cores adicionadas

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,10 +1,62 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:esferapro/screens/dashbord.dart';
 import 'package:flutter/material.dart';
-import 'dashbord.dart';
 import './stack_pages/register_screen.dart'; // Importando a tela de cadastro
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:convert';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget{
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _loginScreen();
+}
+
+class _loginScreen extends State<LoginScreen> {
+  TextEditingController _email = TextEditingController();
+  TextEditingController _password = TextEditingController();
+  Color borderColor = Colors.purple;
+  String _error = '';
+  
+
+  void _validateUser(BuildContext context) async {
+    String email = _email.text;
+    String password = _password.text;
+
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    final url = Uri.parse('http://localhost:8080/login');
+
+    final Map<String, dynamic> dados = {'email': email, 'password': password};
+
+    try {
+      final response = await http.post(
+        url,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: jsonEncode(dados),
+      );
+
+      if (response.statusCode == 200) {
+        await prefs.setInt('userId', json.decode(response.body)['idUser']);
+        Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => MainScreen()), 
+      );
+      } else {
+        print(_error);
+        setState((){
+          _error = json.decode(response.body)['message'];
+          borderColor = Colors.red;
+        });
+      }
+    } catch (e) {
+      print('Erro: $e');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     final double containerWidth = MediaQuery.of(context).size.width * 0.9;
@@ -47,16 +99,20 @@ class LoginScreen extends StatelessWidget {
                         CrossAxisAlignment.center, // Centraliza horizontalmente
                     children: [
                       Container(
-                        width: containerWidth * 0.7, // Largura igual aos campos de texto
+                        width: containerWidth *
+                            0.7, // Largura igual aos campos de texto
                         height: containerHeight, // Altura de 70% da largura
                         decoration: BoxDecoration(
                           color: Colors.grey[300], // Cor do placeholder
-                          borderRadius: BorderRadius.circular(8.0), // Cantos arredondados
+                          borderRadius:
+                              BorderRadius.circular(8.0), // Cantos arredondados
                         ),
                         child: Center(
                           child: Icon(
-                            Icons.image, // Ícone de imagem para representar o logo
-                            size: containerHeight * 0.5, // Ajusta o tamanho do ícone
+                            Icons
+                                .image, // Ícone de imagem para representar o logo
+                            size: containerHeight *
+                                0.5, // Ajusta o tamanho do ícone
                             color: Colors.grey[600],
                           ),
                         ),
@@ -65,38 +121,44 @@ class LoginScreen extends StatelessWidget {
                           height:
                               50.0), // Espaço entre o placeholder e o campo de texto
                       TextField(
+                        controller: _email,
                         decoration: InputDecoration(
-                          labelText: 'Usuários',
+                          labelText: 'Email',
                           border: OutlineInputBorder(),
                           focusedBorder: OutlineInputBorder(
                             borderSide:
-                                BorderSide(color: Colors.purple, width: 2.0),
+                                BorderSide(color: borderColor, width: 2.0),
                           ),
                           enabledBorder: OutlineInputBorder(
                             borderSide:
-                                BorderSide(color: Colors.purple, width: 1.0),
+                                BorderSide(color: borderColor, width: 1.0),
                           ),
                         ),
                       ),
                       SizedBox(height: 20.0),
                       TextField(
+                        controller: _password,
                         decoration: InputDecoration(
                           labelText: 'Senha',
                           border: OutlineInputBorder(),
                           focusedBorder: OutlineInputBorder(
                             borderSide:
-                                BorderSide(color: Colors.purple, width: 2.0),
+                                BorderSide(color: borderColor, width: 2.0),
                           ),
                           enabledBorder: OutlineInputBorder(
                             borderSide:
-                                BorderSide(color: Colors.purple, width: 1.0),
+                                BorderSide(color: borderColor, width: 1.0),
                           ),
                         ),
                         obscureText: true,
                       ),
+                      Text(_error,
+                        style: TextStyle(color: Colors.red),
+                      ),
                       SizedBox(height: 24.0),
                       Container(
-                        width: MediaQuery.of(context).size.width * 0.5, // Largura total
+                        width: MediaQuery.of(context).size.width *
+                            0.5, // Largura total
                         child: ClipRRect(
                           borderRadius: BorderRadius.circular(
                               5), // Aplicar cantos arredondados
@@ -132,11 +194,7 @@ class LoginScreen extends StatelessWidget {
                                     50), // Largura total e altura de 50
                               ),
                               onPressed: () {
-                                Navigator.of(context).pushReplacement(
-                                  MaterialPageRoute(
-                                    builder: (context) => MainScreen(),
-                                  ),
-                                );
+                                _validateUser(context);
                               },
                               child: Text('Entrar'),
                             ),
