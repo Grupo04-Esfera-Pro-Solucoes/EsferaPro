687cd4b1b32b29dc2517768ac408fd0cd9269ef7
Igorggwp <igorggwpp@gmail.com>
2024-09-16 20:31:22 -0300

    finalizacao da tela

--- lib/service/login_service.dart
+++ lib/service/login_service.dart
@@ -1,28 +1,31 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 
-class LoginService {
-  final String _baseUrl = 'http://localhost:8080/login';
+class AuthService {
+  final String _baseUrl = 'http://localhost:8080';
 
   Future<bool> login(String email, String password) async {
-    final Uri url = Uri.parse(_baseUrl);
+    final url = Uri.parse('$_baseUrl/login');
+    final Map<String, dynamic> dados = {'email': email, 'password': password};
 
-    final response = await http.post(
-      url,
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: jsonEncode({
-        'email': email,
-        'password': password,
-      }),
-    );
+    try {
+      final response = await http.post(
+        url,
+        headers: {'Content-Type': 'application/json'},
+        body: jsonEncode(dados),
+      );
 
-    if (response.statusCode == 200) {
-      final Map<String, dynamic> data = jsonDecode(response.body);
-      return data['success'] as bool;
-    } else {
-      throw Exception('Falha ao fazer login: ${response.reasonPhrase}');
+      if (response.statusCode == 200) {
+        final prefs = await SharedPreferences.getInstance();
+        await prefs.setInt('userId', json.decode(response.body)['idUser']);
+        return true;
+      } else {
+        return false;
+      }
+    } catch (e) {
+      print('Erro: $e');
+      return false;
     }
   }
 }
