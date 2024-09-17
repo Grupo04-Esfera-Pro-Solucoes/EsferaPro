f6384e143d01c021d1a1bea1f22c0ef47ba83ac1
Gustavo Dechotti <75210025+GusDechotti@users.noreply.github.com>
2024-09-17 00:55:54 -0300

    Botao para direcinar para stack ok

--- lib/screens/stack_pages/stack_clients.dart
+++ lib/screens/stack_pages/stack_clients.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class StackPage2 extends StatelessWidget {
+class StackClients extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
